28c0295b12d84baa9bd6e2877bc8e3cb1209991a
mohfofana <mohamed-aboubakar-sidiki.fofana@epitech.eu>
2025-07-21 13:03:28 +0200

    redesigned the ui of the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React, { useState, useMemo } from 'react';
-import { ThemeProvider, CssBaseline, AppBar, Toolbar, Typography, IconButton } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
+import { ThemeProvider, CssBaseline, Toolbar } from '@mui/material';
 import { getDesignTokens } from './theme';
+import { createTheme } from '@mui/material/styles';
+import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import NewTicket from './pages/NewTicket';
 import TicketDetails from './pages/TicketDetails';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 function App() {
   const [mode, setMode] = useState<'light' | 'dark'>('light');
@@ -22,17 +21,8 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-        <AppBar position="fixed">
-          <Toolbar>
-            <Typography variant="h6" sx={{ flexGrow: 1 }}>
-              Revolte - Atelier
-            </Typography>
-            <IconButton color="inherit" onClick={toggleColorMode}>
-              {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Toolbar /> {/* Espace égal à la hauteur de la Toolbar pour compenser la navbar fixe */}
+        <Navbar toggleColorMode={toggleColorMode} mode={mode} />
+        <Toolbar /> {/* espace pour la navbar fixe */}
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/new-ticket" element={<NewTicket />} />
